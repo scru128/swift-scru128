b9dd2019e4f122bfba4e691863980ddcd834bf49
LiosK <contact@mail.liosk.net>
2022-04-10 20:41:04 +0900

    refactor: change counter overflow handling strategy

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,17 +9,16 @@
   24 bits, `per_gen_random`: 32 bits } -> { `timestamp`: 48 bits, `counter_hi`:
   24 bits, `counter_lo`: 24 bits, `entropy`: 32 bits }
 - Timestamp epoch: 2020-01-01 00:00:00.000 UTC -> 1970-01-01 00:00:00.000 UTC
-- Custom logger: package-wide configuration -> per-generator configuration
+- Counter overflow handling: stall generator -> increment timestamp
+
+### Removed
+
+- `setScru128Logger()` as counter overflow is no longer likely to occur
+- `Scru128Id#counter`, `Scru128Id#perSecRandom`, `Scru128Id#perGenRandom`
 
 ### Added
 
 - `Scru128Id#counterHi`, `Scru128Id#counterLo`, `Scru128Id#entropy`
-- `Scru128Generator#setLogger()`
-
-### Removed
-
-- `Scru128Id#counter`, `Scru128Id#perSecRandom`, `Scru128Id#perGenRandom`
-- `setScru128Logger()`
 
 ## v1.0.0 - 2022-01-03
 
--- Sources/Scru128/Scru128Generator.swift
+++ Sources/Scru128/Scru128Generator.swift
@@ -13,9 +13,6 @@ public class Scru128Generator {
   /// Random number generator used by the generator.
   private var rng: RandomNumberGenerator
 
-  /// Logger object used by the generator.
-  private var logger: Scru128Logging?
-
   private let lock: NSLocking = NSLock()
 
   /// Creates a generator object with the default random number generator.
@@ -35,73 +32,39 @@ public class Scru128Generator {
   public func generate() -> Scru128Id {
     lock.lock()
     defer { lock.unlock() }
-    while true {
-      do {
-        return try generateCore()
-      } catch is CounterOverflowError {
-        handleCounterOverflow()
-      } catch {
-        fatalError("unexpected error")
-      }
-    }
+    return generateThreadUnsafe()
   }
 
-  /// Generates a new SCRU128 ID object, while delegating the caller to take care of thread safety
-  /// and counter overflows.
-  private func generateCore() throws -> Scru128Id {
+  /// Generates a new SCRU128 ID object without overhead for thread safety.
+  private func generateThreadUnsafe() -> Scru128Id {
     let ts = UInt64(Date().timeIntervalSince1970 * 1_000)
     if ts > timestamp {
       timestamp = ts
       counterLo = rng.next() & maxCounterLo
-      if ts - tsCounterHi >= 1000 {
-        tsCounterHi = ts
-        counterHi = rng.next() & maxCounterHi
-      }
-    } else {
+    } else if ts + 10_000 > timestamp {
       counterLo += 1
       if counterLo > maxCounterLo {
         counterLo = 0
         counterHi += 1
         if counterHi > maxCounterHi {
           counterHi = 0
-          throw CounterOverflowError()
+          // increment timestamp at counter overflow
+          timestamp += 1
+          counterLo = rng.next() & maxCounterLo
         }
       }
+    } else {
+      // reset state if clock moves back more than ten seconds
+      tsCounterHi = 0
+      timestamp = ts
+      counterLo = rng.next() & maxCounterLo
+    }
+
+    if timestamp - tsCounterHi >= 1_000 {
+      tsCounterHi = timestamp
+      counterHi = rng.next() & maxCounterHi
     }
 
     return Scru128Id(timestamp, counterHi, counterLo, rng.next())
   }
-
-  /// Defines the behavior on counter overflow.
-  ///
-  /// Currently, this method waits for the next clock tick and, if the clock does not move forward
-  /// for a while, reinitializes the generator state.
-  private func handleCounterOverflow() {
-    logger?.notice("counter overflowing; will wait for next clock tick")
-    tsCounterHi = 0
-    for _ in 0..<10_000 {
-      Thread.sleep(forTimeInterval: 0.0001)  // 100 microseconds
-      if UInt64(Date().timeIntervalSince1970 * 1_000) > timestamp {
-        return
-      }
-    }
-    logger?.notice("reset state as clock did not move for a while")
-    timestamp = 0
-  }
-
-  /// Specifies the logger object used by the generator.
-  ///
-  /// Logging is disabled by default. Set a logger object to enable logging.
-  public func setLogger(_ newLogger: Scru128Logging) {
-    logger = newLogger
-  }
 }
-
-/// Defines the logger interface used by the generator.
-public protocol Scru128Logging {
-  /// Logs message at default level.
-  func notice(_ message: String)
-}
-
-/// Error thrown when the monotonic counters can no more be incremented.
-private struct CounterOverflowError: Error {}
