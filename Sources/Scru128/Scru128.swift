1a9fa11d5b36d7beebcc8a08ad1298b6aec33c9d
LiosK <contact@mail.liosk.net>
2023-03-15 22:48:31 +0900

    refactor: rename default generator to global generator

--- Sources/Scru128/Scru128.swift
+++ Sources/Scru128/Scru128.swift
@@ -7,14 +7,15 @@ let maxCounterHi: UInt32 = 0xff_ffff
 /// Maximum value of 24-bit `counter_lo` field.
 let maxCounterLo: UInt32 = 0xff_ffff
 
-private let defaultGenerator = Scru128Generator()
+private let globalGenerator = Scru128Generator()
 
-/// Generates a new SCRU128 ID object.
+/// Generates a new SCRU128 ID object using the global generator.
 ///
 /// This function is thread-safe; multiple threads can call it concurrently.
-public func scru128() -> Scru128Id { defaultGenerator.generate() }
+public func scru128() -> Scru128Id { globalGenerator.generate() }
 
-/// Generates a new SCRU128 ID encoded in the 25-digit canonical string representation.
+/// Generates a new SCRU128 ID encoded in the 25-digit canonical string representation using the
+/// global generator.
 ///
 /// This function is thread-safe. Use this to quickly get a new SCRU128 ID as a string.
 public func scru128String() -> String { scru128().description }
